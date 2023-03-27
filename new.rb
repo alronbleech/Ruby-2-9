276c0e6488a06825e823f98395fea0dd0406cbc9
alronbleech <126852030+alronbleech@users.noreply.github.com>
2023-03-27 09:45:14 +0000

    [Finish]2-9

--- new.rb
+++ new.rb
@@ -18,9 +18,13 @@ car = Car.new
 car.move("右",5)
 
 class Car
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
   def self.run(distance)
     puts "車で#{distance}キロ走ります。"
   end
 end
 
+Car.turn("右")
 Car.run(10)
